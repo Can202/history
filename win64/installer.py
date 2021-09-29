84518dfa706dfab61c043eaa61f692c79a8c4611
Can202 <mgoopazo@gmail.com>
2021-09-29 11:48:49 -0300

    installer

--- win64/installer.py
+++ win64/installer.py
@@ -11,4 +11,11 @@ with open("C:\\hyst\\hyst-compiler.exe", 'wb') as f:
     f.write(fil.content)
     f.close()
 
+fil = requests.get("https://raw.githubusercontent.com/Can202/hyst-compiler/a6034b4aa62efd3b2d91145f076b0ce13df5e187/win64/windows.py")
+with open("C:\\hyst\\hyst-compiler.py", 'wb') as f:
+    f.write(fil.content)
+    f.close()
+
+print("Now you can add C:\\hyst\\ to PATH")
+
 
