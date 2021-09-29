efeb0aed46948c5520a9bbdb2caa75d6c4cc9f2b
Can202 <mgoopazo@gmail.com>
2021-09-28 22:21:27 -0300

    from import

--- hystory.py
+++ hystory.py
@@ -33,86 +33,93 @@ if len(sys.argv) > 1:
                 if len(commands[i]) > 0:
                     while commands[i].startswith(" ") or commands[i].startswith("\n"):
                         commands[i] = commands[i][1:len(commands[i])]
-                commands[i] = commands[i].replace(",", ".")
-                if commands[i].lower().startswith("protagonist is "):
-                    func = commands[i][len("protagonist is "):len(commands[i])]
-                    protagonistname = func
-                if commands[i].lower().startswith("friend is "):
-                    func = commands[i][len("friend is "):len(commands[i])]
-                    friendname = func
-                if commands[i].lower().startswith("librarian is "):
-                    func = commands[i][len("librarian is "):len(commands[i])]
-                    librarianname = func
-                if commands[i].lower().startswith("new history: "):
-                    func = commands[i][len("New History: "):len(commands[i])]
-                    if " With " in commands[i]:
-                        parameter = func[func.find(" With ") + 6: len(func)]
-                        func = func[0:func.find(" With ")]
-                        save += init + "def " + func + "(" + parameter + "):"
-                    else:
-                        save += init + "def " + func + "():"
-                    save += "\n"
-                    init += "    "
-                if commands[i].lower().startswith("principal"):
-                    save += init + "def main():"
-                    save += "\n"
-                    init += "    "
-                if commands[i].startswith("Now go to "):
-                    func = commands[i][len("Now go to "):len(commands[i])]
-                    if " With " in commands[i]:
-                        parameter = func[func.find(" With ") + 6: len(func)]
-                        func = func[0:func.find(" With ")]
-                        save += init + func + "(" + parameter + ")"
-                    else:
-                        save += init + func + "()"
-                    save += "\n"
-                elif commands[i].lower().startswith("narrador says "):
+                if commands[i].lower().startswith("narrador says "):
                     newlines = commands[i].split("\n")
                     for j in range(len(newlines)):
                         if newlines[j].lower().startswith("narrador says "):
                             newlines[j]= newlines[j][14:len(newlines[j])]
                         save += init + "# " + newlines[j]
                         save += "\n"
-                elif commands[i].startswith("Hi "):
-                    var = commands[i][3:len(commands[i])]
-                    variables.append(var)
-                    save += init + var + " = None"
-                    save += "\n"
-                elif commands[i].startswith(protagonistname + " says "):
-                    var = commands[i][len(protagonistname + " says "):len(commands[i])]
-                    save += init + "print(\"" + var + "\")"
-                    save += "\n"
-                elif commands[i].startswith(protagonistname + " repeat "):
-                    var = commands[i][len(protagonistname + " repeat "):len(commands[i])]
-                    save += init + "print(" + var + ")"
-                    save += "\n"
-                elif commands[i].startswith(librarianname + " brings "):
-                    var = commands[i][len(librarianname + " brings "):len(commands[i])]
-                    save += init + "import " + var
-                    save += "\n"
-                elif commands[i].startswith(friendname + " ask to " + protagonistname + " about "):
-                    var = commands[i][len(friendname + " ask to " + protagonistname + " about "):len(commands[i])]
-                    save += init + friendname + " = input(\"" + var + "\")"
-                    save += "\n"
-                elif commands[i].startswith(friendname + " ask to " + protagonistname):
-                    save += init + friendname + " = input()"
-                    save += "\n"
-                elif commands[i].startswith("End"):
-                    if len(commands[i]) <= 3:
-                        save += init + "return 0"
-                        save += "\n"
-                        init = init[:-4]
-                    else:
-                        var = commands[i][4:len(commands[i])]
-                        save += init + "return " + var
-                        save += "\n"
-                        init = init[:-4]
                 else:
-                    for u in range(len(variables)):
-                        if commands[i].startswith(variables[u] + " is "):
-                            var = commands[i][len(variables[u] + " is "):len(commands[i])]
-                            save += init + variables[u] + " = " + var
+                    commands[i] = commands[i].replace(",", ".")
+                    if commands[i].lower().startswith("protagonist is "):
+                        func = commands[i][len("protagonist is "):len(commands[i])]
+                        protagonistname = func
+                    elif commands[i].lower().startswith("friend is "):
+                        func = commands[i][len("friend is "):len(commands[i])]
+                        friendname = func
+                    elif commands[i].lower().startswith("librarian is "):
+                        func = commands[i][len("librarian is "):len(commands[i])]
+                        librarianname = func
+                    elif commands[i].lower().startswith("new history: "):
+                        func = commands[i][len("New History: "):len(commands[i])]
+                        if " With " in commands[i]:
+                            parameter = func[func.find(" With ") + 6: len(func)]
+                            func = func[0:func.find(" With ")]
+                            save += init + "def " + func + "(" + parameter + "):"
+                        else:
+                            save += init + "def " + func + "():"
+                        save += "\n"
+                        init += "    "
+                    elif commands[i].lower().startswith("principal"):
+                        save += init + "def main():"
+                        save += "\n"
+                        init += "    "
+                    elif commands[i].startswith("Now go to "):
+                        func = commands[i][len("Now go to "):len(commands[i])]
+                        if " With " in commands[i]:
+                            parameter = func[func.find(" With ") + 6: len(func)]
+                            func = func[0:func.find(" With ")]
+                            save += init + func + "(" + parameter + ")"
+                        else:
+                            save += init + func + "()"
+                        save += "\n"
+                    elif commands[i].startswith("Hi "):
+                        var = commands[i][3:len(commands[i])]
+                        variables.append(var)
+                        save += init + var + " = None"
+                        save += "\n"
+                    elif commands[i].startswith(protagonistname + " says "):
+                        var = commands[i][len(protagonistname + " says "):len(commands[i])]
+                        save += init + "print(\"" + var + "\")"
+                        save += "\n"
+                    elif commands[i].startswith(protagonistname + " repeat "):
+                        var = commands[i][len(protagonistname + " repeat "):len(commands[i])]
+                        save += init + "print(" + var + ")"
+                        save += "\n"
+                    elif commands[i].startswith(librarianname + " brings "):
+                        var = commands[i][len(librarianname + " brings "):len(commands[i])]
+                        save += init + "import " + var
+                        save += "\n"
+                    elif commands[i].startswith(librarianname + " from "):
+                        var = commands[i][len(librarianname + " from "):len(commands[i])]
+                        var = var[0:var.find(" brings")]
+                        libs = commands[i][commands[i].find(" brings ") + 8: len(commands[i])]
+                        save += init + "from " + var + " import " + libs
+                        save += "\n"
+                    elif commands[i].startswith(friendname + " ask to " + protagonistname + " about "):
+                        var = commands[i][len(friendname + " ask to " + protagonistname + " about "):len(commands[i])]
+                        save += init + friendname + " = input(\"" + var + "\")"
+                        save += "\n"
+                    elif commands[i].startswith(friendname + " ask to " + protagonistname):
+                        save += init + friendname + " = input()"
+                        save += "\n"
+                    elif commands[i].startswith("End"):
+                        if len(commands[i]) <= 3:
+                            save += init + "return 0"
                             save += "\n"
+                            init = init[:-4]
+                        else:
+                            var = commands[i][4:len(commands[i])]
+                            save += init + "return " + var
+                            save += "\n"
+                            init = init[:-4]
+                    else:
+                        for u in range(len(variables)):
+                            if commands[i].startswith(variables[u] + " is "):
+                                var = commands[i][len(variables[u] + " is "):len(commands[i])]
+                                save += init + variables[u] + " = " + var
+                                save += "\n"
             save += "main()"
             save += "\n"
             exp = open(savepath, "w")
